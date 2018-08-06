109f76990a65add4ab4c7c0004e45e05c15195a1
Lois Rilo <lois.rilo@eficent.com>
2018-08-06 11:54:25 +0200

    [9.0][MIG] mail_activity_board: backport from 11.0

--- mail_activity_board/models/mail_activity.py
+++ mail_activity_board/models/mail_activity.py
@@ -1,6 +1,6 @@
 # Copyright 2018 David Juaneda - <djuaneda@sdi.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import api, models, fields
+from openerp import api, models, fields
 
 
 class MailActivity(models.Model):
--- mail_activity_board/readme/CONTRIBUTORS.rst
+++ mail_activity_board/readme/CONTRIBUTORS.rst
@@ -1,3 +1,7 @@
 * `SDI <https://www.sdi.es>`_:
 
   * David Juaneda
+
+* `Eficent <http://www.eficent.com>`_:
+
+  * Lois Rilo <lois.rilo@eficent.com>
