619546d3e5af250f8626f1305e94d7d74a3ca2ca
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-08-15 07:23:56 +0200

    fix issue with activities board

--- mail_activity_board/static/src/js/override_chatter.js
+++ mail_activity_board/static/src/js/override_chatter.js
@@ -4,18 +4,19 @@
 odoo.define('mail.Chatter.activity', function(require){
     "use strict";
 
-    var chatter = require('mail.Chatter');
+    var Chatter = require('mail.Chatter');
     var core = require('web.core');
     var Model = require('web.DataModel');
     var _t = core._t;
     var QWeb = core.qweb;
 
-    chatter.include({
-
-        events: {
-            'click .o_chatter_button_list_activity': '_onListActivity',
+    Chatter.include({
+        start: function() {
+            this.$('button.o_chatter_button_list_activity').click(
+                this.proxy('_onListActivity')
+            );
+            return this._super.apply(this, arguments);
         },
-
         _onListActivity: function (event) {
             var res_model = this.view.dataset.model;
             var res_id = this.view.datarecord.id;
